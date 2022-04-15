95c953aafecd917dab27f14871feae69d983afdb
RonHidekiK <hideki.kawachi17@gmail.com>
2022-04-15 21:13:00 +0800

    Header and Footer drafts
    + Created Footer and Header Template/layouts
    + Draft of javascript button implementation header
    + Added temporary links for footer socials buttons

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function buttonForum(){
+    console.log("Forum");
+}
+
+function buttonCreatePost(){
+    console.log("Create Post");
+}
+
+function buttonLogIn(){
+    console.log("Log in");
+}
+
+function buttonRegister(){
+    console.log("Register");
+}
