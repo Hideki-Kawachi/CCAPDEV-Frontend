5f17346c03937108e12c094f5bd5be1499e8a107
RonHidekiK <hideki.kawachi17@gmail.com>
2022-04-16 22:38:54 +0800

    Changed classname
    Changed class names to be more consistent

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
-    state = {} 
+
     render() { 
         return (
-            <div className='popup-box'>
-                <span className='login-text'>Log In</span>
-                <form className='login-details'>
-                    <input type={'text'} placeholder='Email'></input>
-                    <input type={'password'} placeholder='Password'></input>
-                    <button type={'submit'}>Submit</button>
-                </form>
+            <div className='modal-container'>
+                <div className='modal'>
+                    <span className='login-text'>Log In</span>
+                    <form className='login-details'>
+                        <input type={'text'} placeholder='Email'></input>
+                        <input type={'password'} placeholder='Password'></input>
+                        <button type={'submit'} id='login-submit'>Submit</button>
+                    </form>
+                </div>
             </div>
         );
     }
+
 }
  
 export default Login
