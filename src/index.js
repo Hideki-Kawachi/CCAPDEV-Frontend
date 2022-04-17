6e00b12a35d54fa4bb682d1fe8351f389fe59fdb
RonHidekiK <hideki.kawachi17@gmail.com>
2022-04-17 21:35:59 +0800

    Update index.js
    Updated to only contain main render and imports.

--- src/index.js
+++ src/index.js
@@ -1,33 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Login from './components/Login'
+import Home from './components/Home';
+import NavBar from "./components/NavBar";
+import Forum from "./components/Forum";
+import Post from "./components/Post";
+import LogIn from "./components/LogIn";
+import Register from "./components/Register";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import index from './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render();
-
-//initialize event listeners for header buttons
-document.getElementById('headerForum').addEventListener('click',buttonForum);
-document.getElementById('headerCreatePost').addEventListener('click',buttonCreatePost);
-document.getElementById('headerLogIn').addEventListener('click',buttonLogIn);
-document.getElementById('headerRegister').addEventListener('click',buttonRegister);
-
-function buttonForum(){
-  console.log("Forum");
-}
-
-function buttonCreatePost(){
-  console.log("Create Post");
-}
-
-function buttonLogIn(){
-  root.render(<Login></Login>)
-  console.log("Log in");
-}
-
-function buttonRegister(){
-  console.log("Register");
-}
+root.render(
+        <Router>
+            <NavBar></NavBar>
+            <Routes>
+                <Route path="/Forum" element={<Forum></Forum>}></Route>
+                <Route path="/Post" element={<Post></Post>}></Route>
+                <Route path="/Home" element={<Home></Home>}></Route>
+                <Route path="/LogIn" element={<LogIn></LogIn>}></Route>
+                <Route path="/Register" element={<Register></Register>}></Route>
+            </Routes>
+            <footer>
+                <div className="contact-container">
+                    <span className="contact-us">Contact us</span>
+                    <div className="socials">
+                        <a className="facebook" href="https://www.facebook.com/" target="_blank" rel="noreferrer"></a>
+                        <a className="twitter" href="https://twitter.com/" target="_blank" rel="noreferrer"></a>
+                        <a className="instagram" href="https://instagram.com/" target="_blank" rel="noreferrer"></a>
+                    </div>
+                </div>
+            </footer>
+        </Router>
+        );
 
 reportWebVitals();
