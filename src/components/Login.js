b7d4281ffbbbcd138e6853dff1122b7b3bf3771d
RonHidekiK <hideki.kawachi17@gmail.com>
2022-04-18 15:07:11 +0800

    Included Login States
    Email and password states currently working, isLoggedIn is not updating

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import NavBar from './NavBar';
+import { useNavigate } from 'react-router-dom';
+
 
 const Login=()=>{
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    
+    const navigate = useNavigate();
+
+    let validateForm=()=>{
+        return (password.length>0 && email.length>0);
+    };
+
+    let submitForm=(e)=>{
+        navigate("/");
+        e.preventDefault();
+        setIsLoggedIn(true);
+        console.log(email  + "    " + password + "    " + isLoggedIn);
+    }
+
+    useEffect(() => {
+        NavBar({isLoggedIn: isLoggedIn},
+            console.log(isLoggedIn));
+    },[isLoggedIn]);
+
     return (
         <div className='modal-container' id='modal-container-login'>
             <div className='modal'>
                 <span className='login-text'>Log In</span>
-                <form className='login-details'>
-                    <input type={'text'} placeholder='Email'></input>
-                    <input type={'password'} placeholder='Password'></input>
-                    <button type={'submit'} id='login-submit'>Submit</button>
+                <form className='login-details' onSubmit={submitForm}>
+                    <input type={'text'} placeholder='Email' value={email} onChange={(e)=>setEmail(e.target.value)}></input>
+                    <input type={'password'} placeholder='Password' value={password} onChange={(e)=>setPassword(e.target.value)}></input>
+                    <button type={'submit'} id='login-submit' disabled={!validateForm()}>Submit</button>
                 </form>
             </div>
         </div>
