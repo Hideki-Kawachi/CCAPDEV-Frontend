e2f7b72e25ea28c235fc70f533b166118f1f2f79
RonHidekiK <hideki.kawachi17@gmail.com>
2022-04-18 20:26:13 +0800

    Fixed login
    Fixed login status and successful passing of data from login to navbar

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,39 +1,31 @@
-import React, { useEffect, useState } from 'react';
-import NavBar from './NavBar';
+import React, { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 
-const Login=()=>{
+const Login=(props)=>{
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    
+
     const navigate = useNavigate();
 
     let validateForm=()=>{
         return (password.length>0 && email.length>0);
     };
-
-    let submitForm=(e)=>{
-        navigate("/");
+    
+    const sendLogin=(e)=>{
         e.preventDefault();
-        setIsLoggedIn(true);
-        console.log(email  + "    " + password + "    " + isLoggedIn);
+        navigate('/');
+        props.loginChange(true,email);      //change email to username
     }
-
-    useEffect(() => {
-        NavBar({isLoggedIn: isLoggedIn},
-            console.log(isLoggedIn));
-    },[isLoggedIn]);
 
     return (
         <div className='modal-container' id='modal-container-login'>
             <div className='modal'>
                 <span className='login-text'>Log In</span>
-                <form className='login-details' onSubmit={submitForm}>
+                <form className='login-details'>
                     <input type={'text'} placeholder='Email' value={email} onChange={(e)=>setEmail(e.target.value)}></input>
                     <input type={'password'} placeholder='Password' value={password} onChange={(e)=>setPassword(e.target.value)}></input>
-                    <button type={'submit'} id='login-submit' disabled={!validateForm()}>Submit</button>
+                    <button type={'button'} id='login-submit' disabled={!validateForm()} onClick={(e)=>sendLogin(e)}>Submit</button>
                 </form>
             </div>
         </div>
