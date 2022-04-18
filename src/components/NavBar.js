69986383a251c41a739c32555bdb9736f15f9a32
RonHidekiK <hideki.kawachi17@gmail.com>
2022-04-18 15:08:03 +0800

    Updated NavBar
    Included feature to show if there is a user signed in or not in the NavBar.  Currently working but isLoggedIn is not automatically updating

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-const NavBar = () =>{
+
+
+const NavBar = (props) =>{
+    
+    const rightNav = {
+        true: (
+            <div className='right-header'>
+                <p>IS LOGGED IN!</p>
+            </div>),
+        false: (
+            <div className='right-header'>
+                <Link id="headerLogin" className='nav-button' to={'/Login'}>Log in</Link>
+                <Link id="headerRegister" className='nav-button' to={'/Register'}>Register</Link>
+            </div>
+        )
+    }
+
+    console.log("UPDATED NAV BAR  " + props.isLoggedIn);
+
     return(
         <header>
             <div className='left-header'>
@@ -9,12 +27,9 @@ const NavBar = () =>{
                 <Link id="headerCreatePost" className='nav-button' to={'/Post'}>Create a Post</Link>
             </div>
             
-            <Link id="headerHome" to={'/Home'}></Link>
+            <Link id="headerHome" to={'/'}></Link>
 
-            <div className='right-header'>
-                <Link id="headerLogIn" className='nav-button' to={'/LogIn'}>Log in</Link>
-                <Link id="headerRegister" className='nav-button' to={'/Register'}>Register</Link>
-            </div>
+            {rightNav[props.isLoggedIn]}
         </header>
     );
 }
