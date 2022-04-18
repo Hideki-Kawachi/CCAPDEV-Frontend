d13484daa272f66f2e6192fe5436464f5685a547
RonHidekiK <hideki.kawachi17@gmail.com>
2022-04-18 20:27:56 +0800

    Isolated index
    transferred index to app.js

--- src/index.js
+++ src/index.js
@@ -1,39 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-import Home from './components/Home';
-import NavBar from "./components/NavBar";
-import Forum from "./components/Forum";
-import Post from "./components/Post";
-import Login from "./components/Login";
-import Register from "./components/Register";
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import index from './index.css'
-import { useNavigate } from 'react-router-dom';
+import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-        <Router>
-            <NavBar isLoggedIn={false}></NavBar>
-            <Routes>
-                <Route path="/Forum" element={<Forum></Forum>}></Route>
-                <Route path="/Post" element={<Post></Post>}></Route>
-                <Route path="/" element={<Home></Home>}></Route>
-                <Route path="/Login" element={<Login></Login>}></Route>
-                <Route path="/Register" element={<Register></Register>}></Route>
-            </Routes>
-            <footer>
-                <div className="contact-container">
-                    <span className="contact-us">Contact us</span>
-                    <div className="socials">
-                        <a className="facebook" href="https://www.facebook.com/" target="_blank" rel="noreferrer"></a>
-                        <a className="twitter" href="https://twitter.com/" target="_blank" rel="noreferrer"></a>
-                        <a className="instagram" href="https://instagram.com/" target="_blank" rel="noreferrer"></a>
-                    </div>
-                </div>
-            </footer>
-        </Router>
-        );
+root.render(<App></App>);
 
 reportWebVitals();
