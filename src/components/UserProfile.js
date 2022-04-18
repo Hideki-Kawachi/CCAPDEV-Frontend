17160418d5a38740aa039183839b18071ba1df8a
RonHidekiK <hideki.kawachi17@gmail.com>
2022-04-18 20:27:25 +0800

    Create UserProfile.js

---
+++ src/components/UserProfile.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const UserProfile =(props)=>{
+
+    return(
+        <div>
+            <p>{props.username}</p>
+            <p>{props.username}</p>
+            <p>{props.username}</p>
+            <p>{props.username}</p>
+            <p>{props.username}</p>
+            <p>{props.username}</p>
+            <p>{props.username}</p>
+            <p>{props.username}</p>
+            <p>{props.username}</p>
+            <p>{props.username}</p>
+        </div>
+    );
+}
+
+export default UserProfile;
