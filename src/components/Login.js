8ebce92ff202a2c482549dfc209cf77d394656b1
RonHidekiK <hideki.kawachi17@gmail.com>
2022-04-19 12:05:20 +0800

    Updated to accept global context
    made user info a global context instead of props to easily access the user info anywhere in the DOM

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,33 +1,44 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import UserContext from './context/UserContext'
 
 
-const Login=(props)=>{
-    const [email, setEmail] = useState("");
+const Login=()=>{
+    const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const user = useContext(UserContext);
 
     const navigate = useNavigate();
 
     let validateForm=()=>{
-        return (password.length>0 && email.length>0);
+        return (password.length>0 && username.length>0);
     };
-    
+ 
     const sendLogin=(e)=>{
         e.preventDefault();
         navigate('/');
-        props.loginChange(true,email);      //change email to username
+        user.setUsername(username);
+        user.setIsLoggedIn(!user.isLoggedIn);
+        user.setProfilePic(findProfPic(username));
+    }
+
+    const findProfPic=(name)=>{
+        let path = "../media/profilePictures/";
+        path = path.concat(name);
+        path = path.concat(".jpg");
+        return path;
     }
 
     return (
         <div className='modal-container' id='modal-container-login'>
-            <div className='modal'>
-                <span className='login-text'>Log In</span>
-                <form className='login-details'>
-                    <input type={'text'} placeholder='Email' value={email} onChange={(e)=>setEmail(e.target.value)}></input>
-                    <input type={'password'} placeholder='Password' value={password} onChange={(e)=>setPassword(e.target.value)}></input>
-                    <button type={'button'} id='login-submit' disabled={!validateForm()} onClick={(e)=>sendLogin(e)}>Submit</button>
-                </form>
-            </div>
+                <div className='modal'>
+                    <span className='login-text'>Log In</span>
+                    <form className='login-details'>
+                        <input type={'text'} placeholder='Email' value={username} onChange={(e)=>setUsername(e.target.value)}></input>
+                        <input type={'password'} placeholder='Password' value={password} onChange={(e)=>setPassword(e.target.value)}></input>
+                        <button type={'button'} id='login-submit' disabled={!validateForm()} onClick={(e)=>sendLogin(e)}>Submit</button>
+                    </form>
+                </div>
         </div>
     )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import UserContext from './context/UserContext'
 
-const NavBar = (props) =>{
-    const [isLoggedIn, setIsLoggedIn] = useState(props.isLoggedIn);
-    
-    useEffect(() =>{
-        if(props.isLoggedIn !== isLoggedIn){
-            setIsLoggedIn(props.isLoggedIn);
-        }
-    }, [props.isLoggedIn,isLoggedIn])
+const NavBar = () =>{
+    const user = useContext(UserContext);
 
-    const rightNav = {
+    console.log(user);
+    const rightNav = {//profile pic currently not showing but correct directory/file path
         true: (
             <div className='right-header'>
-                <Link id="headerUser" className='nav-button' to={'/UserProfile'}><img src={props.profPicPath}></img></Link>
+                <Link id="headerUser" className='nav-button' to={'/UserProfile'}><img src={user.profilePic}></img></Link>
             </div>),
         false: (
             <div className='right-header'>
@@ -32,7 +28,7 @@ const NavBar = (props) =>{
             
             <Link id="headerHome" to={'/'}></Link>
 
-            {rightNav[isLoggedIn]}
+            {rightNav[user.isLoggedIn]}
         </header>
     );
 }
