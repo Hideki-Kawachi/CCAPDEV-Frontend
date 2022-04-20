7626d8e0f19b206a680251ae8228a9687bc45b52
Cotrii <64764789+Cotrii@users.noreply.github.com>
2022-04-20 16:35:02 +0800

    "added content in Homepage"

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,27 @@ import React from 'react';
 const Home =()=> {
     return (
     <div className='content-home'>
+
+        <div className="sub-container">
+            -------------------
+        </div>
+
+        <div className="name-container">
+            <span className="website-name">PCGuro</span>
+        </div>
+
+        <div className="options-container">
+            <div className='options-bar'>
+                <span>Technical Reviews</span>
+                <span>Local Store Reviews</span>
+                <span>Configuration Guide</span>
+                <span>PC Build Help</span>
+            </div>
+        </div>
+
+        <div className="">
+            
+        </div>
         <p>HELLO</p>
         <p>Home</p>
         <p>Home</p>
