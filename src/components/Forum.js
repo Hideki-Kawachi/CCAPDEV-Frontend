07dd2c3b127dbf1be9d936668e86d4dc22aa5d59
RonHidekiK <hideki.kawachi17@gmail.com>
2022-04-20 11:19:42 +0800

    Updated content

--- src/components/Forum.js
+++ src/components/Forum.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 const Forum = () => {
     return(
-        <div>
+        <div className='content-forum'>
+            <p>HELLO</p>
             <p>FORUM</p>
             <p>FORUM</p>
             <p>FORUM</p>
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 const Home =()=> {
     return (
-    <div>
+    <div className='content-home'>
+        <p>HELLO</p>
         <p>Home</p>
         <p>Home</p>
         <p>Home</p>
