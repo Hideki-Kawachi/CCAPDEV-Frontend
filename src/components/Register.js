720e16b4b2eeb4d9f28db6989ae9b30134f9d67c
RonHidekiK <hideki.kawachi17@gmail.com>
2022-04-23 11:17:44 +0800

    Updated register account
    Started applying account registration validation checking

--- src/components/Register.js
+++ src/components/Register.js
@@ -10,18 +10,39 @@ const Register = () => {
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
 
+    let firstNameError = false;
+
     const user = useContext(UserContext);
     const navigate = useNavigate();
 
-    let validateForm=()=>{
-        return (password.length>0 && username.length>0 && firstName.length>0 && lastName.length>0 && (password===confirmPassword));
-    };
-
-    const sendRegister =()=>{
-        navigate("/");
-        console.log("SENT");
+    const validateForm=()=>{
+        if(password.length>0 && username.length>0 && firstName.length>0 && lastName.length>0 && (password===confirmPassword)){
+            let regEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            if (regEmail.test(email)) {
+                return true;
+            }
+        }
+        firstNameError=true;
+        setUsername("");
+        setEmail("");
+        setPassword("");
+        setConfirmPassword("");
+        setFirstName("");
+        setLastName("");
+        return false;
     }
 
+    const sendRegister =()=>{
+        let isValid = validateForm();
+        if(isValid){
+            navigate("/");
+            console.log("SENT");
+        }
+        else{
+            console.log("error message "+firstNameError);
+        }
+    }
+    
     return(
         <div className='content-register'>
             <div className='register-left'>
@@ -33,7 +54,7 @@ const Register = () => {
                         <span className='font-shadow'>CREATE ACCOUNT</span>
                     </div>
                     <div className='inside-register-right'>
-                        <input type={'firstName'} placeholder='First Name' value={firstName} onChange={(e)=>setFirstName(e.target.value)}></input>
+                        <input type={'firstName'} placeholder='First Name' value={firstName} onChange={(e)=>setFirstName(e.target.value)} style={firstNameError ? {borderColor:'red'}:{}}></input>
                         <input type={'lasttName'} placeholder='Last Name' value={lastName} onChange={(e)=>setLastName(e.target.value)}></input>
                     </div>
                     <div className='inside-register-right'>
@@ -44,7 +65,7 @@ const Register = () => {
                         <input type={'password'} placeholder='Password' value={password} onChange={(e)=>setPassword(e.target.value)}></input>
                         <input type={'Password'} placeholder='Confirm Password' value={confirmPassword} onChange={(e)=>setConfirmPassword(e.target.value)}></input>
                     </div>
-                    <button type={'button'} id='register-submit' disabled={!validateForm()} onClick={sendRegister}>Submit</button>
+                    <button type={'button'} id='register-submit' onClick={sendRegister}>Submit</button>
                 </div>
             </div>
         </div>
