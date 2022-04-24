4f96cd35ee3364c5b6d384141283c8e69fc41990
RonHidekiK <hideki.kawachi17@gmail.com>
2022-04-24 20:31:53 +0800

    Added Store Review Viewing template and back button

--- src/components/StoreReviewView.js
+++ src/components/StoreReviewView.js
@@ -1,16 +1,35 @@
 import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
+import StarRating from './StarRating';
+import { useNavigate } from 'react-router-dom';
 
 function StoreReviewView() {
     const info = useLocation();
 
+    const navigate = useNavigate();
+
+    const back=()=>{
+        navigate(-1);
+        console.log("EXIT FROM LOGIN");
+    }
+
     return (
         <div className='content-store-review-view'>
-          <span>{info.state.title}</span>
-          <span>{info.state.user}</span>
-          <span>{info.state.date}</span>
-          <span>{info.state.description}</span>
-          <span>{info.state.rating}</span>
+            <>
+                <div  className='store-review-view-header'>
+                <button className='back-button' onClick={back}></button>
+                    <span className='store-review-view-title'>{info.state.title}</span>
+                    <StarRating rating = {info.state.rating}></StarRating>
+                </div>
+                <div className='store-review-view-sub-header'>
+                    <span>posted by: </span>
+                    <span className='store-review-view-user'> {info.state.user}</span>
+                    <span className='store-review-view-date'>{info.state.date}</span>
+                </div>
+            </>
+            <div className='store-review-view-description'>
+                <span>{info.state.description}</span>
+            </div>
         </div>
       );
 }
