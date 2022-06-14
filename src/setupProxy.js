548499c44ebee0c950fe6c482346b123958e381c
RonHidekiK <hideki.kawachi17@gmail.com>
2022-06-14 18:53:50 +0800

    proxy for login and register

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,15 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/api',
+    '/Login',
+    createProxyMiddleware({
+      target: 'http://localhost:8080',
+      changeOrigin: true,
+    })
+  );
+
+  app.use(
+    '/Register',
     createProxyMiddleware({
       target: 'http://localhost:8080',
       changeOrigin: true,
