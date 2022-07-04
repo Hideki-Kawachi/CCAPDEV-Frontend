2db76773656bb0fd588dc6f8d12e6f4fc16065cf
Cotrii <carlo_miguel_cortez@dlsu.edu.ph>
2022-07-04 20:53:12 +0800

    2nd update

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -21,17 +21,17 @@ const AboutUs =()=> {
                     display: 'inline-block',
                     alignItems: 'left'
                 }}>
-                        <li> "bcrypt": "^5.0.1"  </li>
-                        <li> "body-parser": "^1.20.0" </li>
-                        <li> "cors": "^2.8.5"</li>
-                        <li> "dotenv": "^16.0.1" </li>
-                        <li> "express": "^4.18.1" </li>
-                        <li> "jsonwebtoken": "^8.5.1" </li>
-                        <li> "mongodb": "^4.6.0" </li>
-                        <li>"mongoose": "^6.3.5"</li>
-                        <li>"mongoose-int32": "^0.6.0"</li>
-                        <li>"multer": "^1.4.2"</li>
-                        <li>"multer-gridfs-storage": "^5.0.2"</li>
+                        <li> bcrypt@5.0.1 </li>
+                        <li> body-parser@1.20.0 </li>
+                        <li> cors@2.8.5 </li>
+                        <li> dotenv@16.0.1 </li>
+                        <li> express@4.18.1 </li>
+                        <li> jsonwebtoken@8.5.1 </li>
+                        <li> mongodb@4.6.0 </li>
+                        <li> mongoose-int32@0.6.0</li>
+                        <li> mongoose@6.3.5</li>
+                        <li> multer-gridfs-storage@5.0.2</li>
+                        <li> multer@1.4.2</li>
                     </ul>
             </div>
             <div className='sub-container'>
@@ -47,18 +47,18 @@ const AboutUs =()=> {
                     display: 'inline-block',
                     alignItems: 'left'
                 }}>
-                        <li> @testing-library/jest-dom": "^5.16.4 </li>
-                        <li> @testing-library/react": "^13.0.1" </li>
-                        <li>  "@testing-library/user-event": "^13.5.0", </li>
-                        <li> "base-64": "^1.0.0", </li>
-                        <li> "buffer": "^6.0.3", </li>
-                        <li> "http-proxy-middleware": "^2.0.6", </li>
-                        <li> "react": "^18.0.0", </li>
-                        <li>"react-dom": "^18.0.0", </li>
-                        <li>"react-router-dom": "^6.3.0", </li>
-                        <li>""react-scripts": "5.0.1", </li>
-                        <li>""react-scripts": "5.0.1","</li>
-                        <li> "xlsx": "^0.18.5"</li>
+                        <li> @testing-library/jest-dom@5.16.4 </li>
+                        <li> @testing-library/react@13.0.1 </li>
+                        <li> @testing-library/user-event@13.5.0, </li>
+                        <li> base-64@1.0.0 </li>
+                        <li> buffer@6.0.3 </li>
+                        <li> http-proxy-middleware@2.0.6 </li>
+                        <li> react-dom@18.0.0 </li>
+                        <li> react-router-dom@6.3.0</li>
+                        <li> react-scripts@5.0.1 </li>
+                        <li> react@18.0.0 </li>
+                        <li> web-vitals@2.1.4</li>
+                        <li> xlsx@0.18.5</li>
                     </ul>
             </div>
 
