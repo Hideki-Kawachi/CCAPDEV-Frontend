f4562d6db0376d2488cba0d40c654016efcc66b7
Cotrii <carlo_miguel_cortez@dlsu.edu.ph>
2022-07-04 20:56:47 +0800

    removed proxy in setupUproxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -144,13 +144,5 @@ module.exports = function(app) {
       changeOrigin: true,
     })
   );
-
-  app.use(
-    '/PAboutUs',
-    createProxyMiddleware({
-      target: 'http://localhost:8080',
-      changeOrigin: true,
-    })
-  );
   
 };
